a8a59792c5510af7dd2c5c96d563f76459c7eef9
TanmayCoder <tanmaydeveloper@gmail.com>
2017-09-28 18:14:51 +0530

    deleting source

--- app/main.ts
+++
@@ -1,8 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { BootModule } from './boot';
-
-enableProdMode();
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(BootModule).then(success => console.log(`Bootstrap success`))
-    .catch(error => console.log(error));
